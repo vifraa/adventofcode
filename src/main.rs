7d1b5a4ba23da94213288ebe9dd131d8a3330c60
Viktor Franzen <viktor@frnzn.com>
2021-11-30 21:13:22 +0100

    Added clap as the cli argument parsing library.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    
+    let matches = App::new("Advent of Code")
+        .author("Viktor Franzén. <viktor@frnzn.com>")
+        .about("Solutions to advent of code problems.")
+        .arg(Arg::new("day")
+             .short('d')
+             .long("day")
+             .about("The day to run")
+             .required(true)
+             .takes_value(true)
+             )
+        .arg(Arg::new("part")
+             .short('p')
+             .long("part")
+             .about("The part of the day to run")
+             .required(true)
+             .takes_value(true))
+        .get_matches();
+
+
+    // Should probably handle these more gracefully than just unwrapping
+    // Also should probably check here if they are of the correct format.
+    let day = matches.value_of("day").unwrap();
+    let part = matches.value_of("part").unwrap();
+
+
+
+
 }
+
