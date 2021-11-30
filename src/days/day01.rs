f461c75b41b506373d4ad53e10cb27155dcf6225
Viktor Franzen <viktor@frnzn.com>
2021-11-30 21:44:38 +0100

    Added base test structure. Will be much code duplication, should probably extract.

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -6,3 +6,36 @@ pub fn part1(input: &str) -> Result<&str, &str> {
 pub fn part2(input: &str) -> Result<&str, &str> {
     Ok("Not implemented")
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = "";
+        let expected = "";
+
+        match part1(input) {
+            Ok(res) => assert_eq!(expected, res),
+            Err(_) => assert!(false)
+        }
+    }
+
+
+    #[test]
+    fn test_2() {
+        let input = "";
+        let expected = "";
+
+        match part2(input) {
+            Ok(res) => assert_eq!(expected, res),
+            Err(_) => assert!(false)
+        }
+    }
+
+}
+
+
