84872acc26971b2a6bcd0d4b9424fa8e9475217a
Viktor Franzen <viktor@frnzn.com>
2021-12-01 07:26:04 +0100

    Solutions to day 1, part 1 and 2.

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,10 +1,50 @@
+use std::os::unix::prelude::MetadataExt;
 
-pub fn part1(input: &str) -> Result<&str, &str> {
-    Ok("Not implemented")
+
+pub fn part1(input: &str) -> Result<String, &str> {
+    let inputs: Vec<i32> = input.lines()
+        .map(|f| f.trim().parse::<i32>().unwrap())
+        .collect();
+
+    let mut result = 0;
+    let mut previous_value: Option<i32> = None;
+    inputs.iter()
+        .for_each(|f| {
+            if let Some(value) = previous_value {
+                if f > &value {
+                    result += 1;
+                }
+            }
+            previous_value = Some(*f);
+        });
+
+    Ok(result.to_string())
 }
 
-pub fn part2(input: &str) -> Result<&str, &str> {
-    Ok("Not implemented")
+pub fn part2(input: &str) -> Result<String, &str> {
+    let inputs: Vec<i32> = input.lines()
+        .map(|f| f.trim().parse().unwrap())
+        .collect();
+    
+    let mut result = 0;
+    let mut previous_value: Option<i32> = None;
+    for (i, value) in inputs.iter().enumerate() {
+        // Check if we are able to create 3 pair
+        if i+2 >= inputs.len() {
+            break;
+        }
+
+        let sum = value + inputs[i+1] + inputs[i+2];
+        if let Some(value) = previous_value {
+            if sum > value {
+                result += 1;
+            }
+        }
+
+        previous_value = Some(sum);
+    }
+
+    Ok(result.to_string())
 }
 
 
@@ -15,8 +55,17 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let input = "";
-        let expected = "";
+        let input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+        let expected = "7";
 
         match part1(input) {
             Ok(res) => assert_eq!(expected, res),
@@ -27,8 +76,18 @@ mod tests {
 
     #[test]
     fn test_2() {
-        let input = "";
-        let expected = "";
+        let input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        let expected = "5";
 
         match part2(input) {
             Ok(res) => assert_eq!(expected, res),
