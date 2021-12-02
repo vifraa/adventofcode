b643ba155a71f0fa9809ec6a48ac7dd0814b6022
Viktor Franzen <viktor@frnzn.com>
2021-12-02 07:06:03 +0100

    Finished implementing day 2.

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,10 +1,53 @@
 
 pub fn part1(input: &str) -> Result<String, &str> {
-    Ok("Not implemented".to_string())
+    let split_input: Vec<Vec<&str>> = input.lines()
+        .map(|f| f.split_whitespace().collect())
+        .collect();
+
+    let horizontal_change: i32 = split_input.iter()
+        .filter(|f| f[0] == "forward")
+        .map(|f| f[1].parse::<i32>().unwrap())
+        .sum();
+
+    let vertical_change: i32 = split_input.iter()
+        .filter(|f| f[0] != "forward")
+        .map(|f| {
+            match f[0] {
+                "down" => f[1].parse::<i32>().unwrap(),
+                "up" => -f[1].parse::<i32>().unwrap(),
+                _ => 0
+            }
+        }).sum();
+
+    Ok((horizontal_change * vertical_change).to_string())
 }
 
 pub fn part2(input: &str) -> Result<String, &str> {
-    Ok("Not implemented".to_string())
+    let split_input: Vec<Vec<&str>> = input.lines()
+        .map(|f| f.split_whitespace().collect())
+        .collect();
+
+
+    let mut aim = 0;
+    let mut horizontal = 0;
+    let mut vertical = 0;
+    for s in split_input {
+        match s[0] {
+            "down" => {
+                aim += s[1].parse::<i32>().unwrap();
+            },
+            "up" => {
+                aim -= s[1].parse::<i32>().unwrap();
+            },
+            "forward" => {
+                horizontal += s[1].parse::<i32>().unwrap();
+                vertical += aim*s[1].parse::<i32>().unwrap();
+            },
+            _ => ()
+        };
+    }
+
+    Ok((horizontal * vertical).to_string())
 }
 
 
@@ -15,8 +58,13 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let input = "";
-        let expected = "";
+        let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+        let expected = "150";
 
         match part1(input) {
             Ok(res) => assert_eq!(expected, res),
@@ -27,8 +75,13 @@ mod tests {
 
     #[test]
     fn test_2() {
-        let input = "";        
-        let expected = "";
+let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+        let expected = "900";
 
         match part2(input) {
             Ok(res) => assert_eq!(expected, res),
